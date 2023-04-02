d2a113083ad40d0fb0525baae4d58a7b124272da
MonsieurBleu <97893210+MonsieurBleu@users.noreply.github.com>
2023-04-02 19:17:41 +0200

    commit

--- include/quadtree.hpp
+++ include/quadtree.hpp
@@ -3,19 +3,63 @@
 
 #include <iostream>
 
-struct atome
-{
+// #define NODE_NON_UNIFORM 0b11111111
+// #define PIXEL_EMPTY 0
+// #define PIXEL_BLACK 1
 
+typedef enum
+{
+    NODE_NON_UNIFORM = 0b11111111,
+    PIXEL_EMPTY = 0,
+    PIXEL_BLACK = 1,
+} Pixel_color;
+
+#define DEFAULT_DEPTH 4
+
+struct Pixel
+{
+    uint8_t color = PIXEL_EMPTY;
 };
 
-struct quadnode
-{
+std::ostream& operator<<(std::ostream& os, const Pixel& p);
 
+struct coord2D
+{
+    int x;
+    int y;
 };
 
-class quadtree
-{
+std::ostream& operator<<(std::ostream& os, const coord2D& p);
 
+struct Quadnode
+{
+    Pixel val = {NODE_NON_UNIFORM};
+    uint8_t depth = 0;
+
+    Quadnode *topl = nullptr;
+    Quadnode *topr = nullptr;
+    Quadnode *botl = nullptr;
+    Quadnode *botr = nullptr;
+
+    ~Quadnode();
+};
+
+class Quadtree
+{
+    private :
+        Quadnode root;
+
+        uint8_t max_depth = DEFAULT_DEPTH;
+        coord2D size;
+        coord2D sizeh;
+
+    public : 
+        Quadtree();
+        void set_max_depth(int newmd);
+        Pixel pixel(coord2D coord);
+        void place(Pixel_color color, coord2D start, coord2D end);
+
+        void generate_debug();
 };
 
 
--- src/quadtree.cpp
+++ src/quadtree.cpp
@@ -1,2 +1,92 @@
 #include <quadtree.hpp>
 
+std::ostream& operator<<(std::ostream& os, const Pixel& p)
+{
+    switch(p.color)
+    {
+    case PIXEL_EMPTY:
+        os << " ";
+        break;
+    
+    case PIXEL_BLACK :
+        os << "#";
+        break;
+    }
+
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const coord2D& c)
+{
+    os << "[" << c.x << ",\t" << c.y << "]";
+    return os;
+}
+
+Quadtree::Quadtree()
+{
+    set_max_depth(DEFAULT_DEPTH);
+}
+
+void Quadtree::set_max_depth(int newmd)
+{
+    max_depth = newmd;
+    size.x = 2<<max_depth;
+    size.y = size.x;
+    sizeh.x = size.x/2;
+    sizeh.y = size.y/2;
+}
+
+Quadnode::~Quadnode()
+{
+    if(topl) delete topl;
+    if(topr) delete topr;
+    if(botl) delete botl;
+    if(botr) delete botr;
+}
+
+void Quadtree::generate_debug()
+{
+    root.topl = new Quadnode;
+    root.topl->val.color = PIXEL_BLACK;
+}
+
+Pixel Quadtree::pixel(coord2D coord)
+{
+    Quadnode *qn = &root;
+
+    while(qn && qn->val.color == NODE_NON_UNIFORM)
+    {
+        if(coord.x < sizeh.x)
+        {
+            if(coord.y < sizeh.y) // bottom left
+            {
+                qn = qn->botl;
+            }
+            else // bottom right
+            {
+                qn = qn->botr;
+            }
+        }
+        else
+        {
+            if(coord.y < sizeh.y) // top left
+            {
+                qn = qn->topl;
+            }
+            else // top right
+            {
+                qn = qn->topr;
+            }
+        }
+    }
+
+    if(qn) return qn->val;
+    
+    Pixel empt;
+    return empt;
+}
+
+void Quadtree::place(Pixel_color color, coord2D start, coord2D end)
+{
+
+}
